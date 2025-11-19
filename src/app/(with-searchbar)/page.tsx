dd85254d5f3677c4a5169eb264e4e0a5115cc4e6
SangWon7242 <gotkdqja@gmail.com>
2025-11-20 02:19:02 +0900

    클라이언트 컴포넌트에서 서버 컴포넌트를 props로 넘겨 렌더링 하면 서버 컴포넌트를 클라이언트 컴퍼넌트로 변환할 일이 없다

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,10 +1,13 @@
 import ClientComponent from "./client-components";
+import ServerComponent from "./server-components";
 
 export default function Home() {
   return (
     <div>
       메인 페이지
-      <ClientComponent />
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
     </div>
   );
 }
