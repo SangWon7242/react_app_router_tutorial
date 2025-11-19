1b880f8a19caa935691473cc6d13a53889c8aac8
SangWon7242 <gotkdqja@gmail.com>
2025-11-20 02:17:46 +0900

    클라이언트 컴포넌트 자식으로 서버 컴포넌트를 두면 해당 컴포넌트는 클라이언트 컴포넌트로 변환된다

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,3 +1,10 @@
+import ClientComponent from "./client-components";
+
 export default function Home() {
-  return <div>메인 페이지</div>;
+  return (
+    <div>
+      메인 페이지
+      <ClientComponent />
+    </div>
+  );
 }
